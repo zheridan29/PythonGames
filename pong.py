d8479e694a571dd9ba4108df58df4615edd94646
zheridan29 <ace.zheridan@gmail.com>
2021-05-25 21:02:10 +0800

    First line of code for the Pong game.

--- pong.py
+++ pong.py
@@ -1,3 +1,15 @@
-print("Hello, Python!")
-print("Ace Gutierrez")
-print("Ace")
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @zheridan29")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+#stops the window from updating
+#so we need to manually update the window
+#and can speed the game in terms of performance
+wn.tracer(0)
+
+#Main game loop
+while True:
+    wn.update()
+
